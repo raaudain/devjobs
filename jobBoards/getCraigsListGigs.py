e8711815d76de2bede82f081aacaa5551d64a451
Ramon Audain <raudain123@gmail.com>
2021-05-02 22:02:30 -0700

    Create json

--- jobBoards/getCraigsListGigs.py
+++ jobBoards/getCraigsListGigs.py
@@ -2,17 +2,23 @@ from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 import requests
 import sys
+import json
 
 
-f = open(f"./locations.txt", "r")
+f = open(f"./data/locations.txt", "r")
 locations = [location.rstrip() for location in f]
 f.close()
 
-m = open(f"./miami.txt", "r")
+m = open(f"./data/miami.txt", "r")
 miamis = [miami.rstrip() for miami in m]
 m.close()
 
-gigs = []
+data = {}
+data["data"] = []
+
+def createJSON(item):
+    with open("./data/data.json", "a", encoding='utf-8') as json_file:
+        json.dump(data, json_file, ensure_ascii=False, indent=4)
 
 def getGigs(item):
     for gig in item:
@@ -23,12 +29,16 @@ def getGigs(item):
         
         age = datetime.timestamp(datetime.now() - timedelta(days=7))
         postDate = datetime.timestamp(datetime.strptime(date, "%Y-%m-%d %H:%M"))
-        
+
         if age <= postDate:
-            g = {"date": date, "title": title, "url": url, "area": area}
-            f = open(f"./data/data.txt", "a")
-            f.write(f"{g}\n")
-            f.close()
+            # createJSON(postDate, title, url, area, "gig")
+            data["data"].append({
+                "timestamp": postDate,
+                "title": title,
+                "url": url,
+                "area": area,
+                "category": "gig"
+            })
 
 def getResults(item):
     soup = BeautifulSoup(item, "lxml")
@@ -50,9 +60,8 @@ def getURLMiami(items):
 def main():
     getURL(locations)
     getURLMiami(miamis)
+    createJSON(data)
 
 main()
 
 sys.exit(0)
-# print(gigs)
-
