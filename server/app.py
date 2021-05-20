0b2dfbfed65a52bf324542568e88f5ec886ab554
Ramon Audain <raudain123@gmail.com>
2021-05-20 10:17:29 -0700

    Update json

--- server/app.py
+++ server/app.py
@@ -3,13 +3,14 @@ from flask import json
 from apscheduler.schedulers.background import BackgroundScheduler
 # from flask_apscheduler import APScheduler
 from job_boards import _main
-import os
+import os, time
 
 # sched = APScheduler()
 app = Flask(__name__, static_folder="data")
 
 def scanner():
     _main.main()
+    print("=> Scanner is set to run")
 
 sched = BackgroundScheduler(daemon=True)
 sched.add_job(scanner, "interval", hours=6)
@@ -24,9 +25,8 @@ def data():
     filename = os.path.join(app.static_folder, "data.json")
     f = open(filename, "r")
     jsonFile = json.load(f)
-    
-    
 
+_main.main()
 
 if __name__ == "__main__":
     # sched.add_job(id="job_scanner", func=scanner, trigger="interval", minutes=30)
--- server/job_boards/_main.py
+++ server/job_boards/_main.py
@@ -1,34 +1,35 @@
 # import craigslist_gigs
 from git import Repo
-# from . import craigslist_jobs
-# # from . import crunchyroll
-# from . import blend 
-# from . import key_values
-# from . import protege
-# from . import nocsok
-# from . import remote_co
-# from . import remoteok
-# from . import weworkremotely
-# from . import builtin
-# from . import workwithindies
-# from .modules import create_temp_json
-# from .modules import create_main_json
-# from datetime import datetime
+from . import craigslist_jobs
+# from . import crunchyroll
+from . import blend 
+from . import key_values
+from . import protege
+from . import nocsok
+from . import remote_co
+from . import remoteok
+from . import weworkremotely
+from . import builtin
+from . import workwithindies
+from .modules import create_temp_json
+from .modules import create_main_json
+from datetime import datetime
 import sys, os
 
 
 def gitPush():
     pathToRepo = f"{os.getcwd()}/.git"
     commitMessage = "Update json"
-    
+
     try:
         repo = Repo(pathToRepo)
         repo.git.add(update=True)
         repo.index.commit(commitMessage)
         origin = repo.remote(name="origin")
         origin.push()
+        print("=> Pushed to GitHub")
     except:
-        print("Some error occured while pushing the code") 
+        print("=> Failed to push to GitHub") 
 
 def main():
     print("=> Scanning job boards")
@@ -52,7 +53,7 @@ def main():
     print("=> Total time: " + str(datetime.now()-start))
 
 # main()
-gitPush()
+# gitPush()
 
 
 # sys.exit(0)
