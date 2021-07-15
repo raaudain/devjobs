4008bc089c1af69c9a3807ae98b8a36015a419ea
Ramon Audain <raudain123@gmail.com>
2021-07-15 10:50:55 -0700

    Update json

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from gevent.pywsgi import WSGIServer
 from flask import json
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -7,28 +8,30 @@ from job_boards import _main
 
 app = Flask(__name__, static_folder="data")
 
+cors = CORS(app, resources={r"/*": {"origins":"*"}})
+
 def scanner():
     print("=> Scanner is set to run")
     _main.main()
 
-sched = BackgroundScheduler(daemon=True)
-# sched.add_job(scanner, "interval", minutes=10)
-sched.add_job(scanner, "cron", hour=4)
-sched.start()
+# sched = BackgroundScheduler(daemon=True)
+# # sched.add_job(scanner, "interval", minutes=10)
+# sched.add_job(scanner, "cron", hour=4)
+# sched.start()
 
 @app.route("/")
 def index():
     return "Hello."
 
-# @app.route("/json")
-def data():
+@app.route("/json")
+def data():    
     with open("./data/data.json", mode="r") as f:
-        text = json.load(f)
+        text = json.dumps(f)
         return text
 
 
 if __name__ == "__main__":
-    # app.run(debug=False)
-    http_server = WSGIServer(("", 5000), app)
-    http_server.serve_forever()
+    app.run(debug=True)
+    # http_server = WSGIServer(("", 5000), app)
+    # http_server.serve_forever()
 
