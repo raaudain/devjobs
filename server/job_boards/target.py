a168e923b96900be9811815465707137abf749ec
Ramon Audain <raudain123@gmail.com>
2021-07-23 23:10:39 -0700

    complete target

--- server/job_boards/target.py
+++ server/job_boards/target.py
@@ -4,11 +4,11 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-# from .modules import create_temp_json
-# from .modules import driver
-import modules.create_temp_json as create_temp_json
-import modules.driver as driver
-import sys, time
+from .modules import create_temp_json
+from .modules import driver
+# import modules.create_temp_json as create_temp_json
+# import modules.driver as driver
+import sys
 
 
 driver = driver.firefox
@@ -16,10 +16,12 @@ driver = driver.firefox
 data = create_temp_json.data
 
 options = webdriver.FirefoxOptions()
-# options.add_argument("--headless")
+options.add_argument("--headless")
 browser = webdriver.Firefox(executable_path=driver, options=options)
 
-wait = WebDriverWait(browser, 30)
+wait = WebDriverWait(browser, 10)
+
+isTrue = True
 
 def getJobs(date, apply_url, company_name, position, locations_string):
     date = str(date)
@@ -44,51 +46,45 @@ def getJobs(date, apply_url, company_name, position, locations_string):
 
 def getResults(item):
     soup = BeautifulSoup(item, "lxml")
-    results = soup.find_all("li", {"data-job-id":True})
-
-    print(results)
+    results = soup.find("section", {"id":"search-results-list"}).find_all("li")
 
     for i in results:
-        date = datetime.strftime(datetime.now(), "%Y-%m-%d")
-        apply_url = "https://jobs.target.com"+i["href"]
-        company_name = "Target"
-        position = i.find("h2").text.strip()
-        locations_string = i.find("span", class_="job-location").text.strip()
-        
-        print(date, apply_url, company_name, position, locations_string)
+        if i.find("h2"):
+            date = datetime.strftime(datetime.now(), "%Y-%m-%d")
+            apply_url = "https://jobs.target.com"+i.find("a", href=True)["href"]
+            company_name = "Target"
+            position = str(i.find("h2")).replace("<h2>", "").replace("</h2>", "").replace("&amp;", "&").strip()
+            locations_string = str(i.find("span", class_="job-location")).replace('<span class="job-location">', "").replace("</span>", "").strip()
+            
+            getJobs(date, apply_url, company_name, position, locations_string)
 
 def getURL():
-    # while True:
-        # if EC.presence_of_element_located((By.XPATH, "//a[@class='next disabled']")):
-        #     break
+    page = 1
 
+    while isTrue:
+        try:
+            url = f"https://jobs.target.com/search-jobs?ac=67611&p={page}"
+
+            browser.get(url)
+
+            wait.until(EC.presence_of_element_located((By.XPATH, "//span[@class='job-location']")))
+
+            response = browser.find_element_by_xpath("//*").get_attribute("outerHTML")
+
+            getResults(response)
+
+            page+=1
+        except:
+            break
         
-    url = f"https://jobs.target.com/search-jobs?ac=67611"
-
-    browser.get(url)
-
-    wait.until(EC.presence_of_element_located((By.XPATH, "//span[@class='job-location']")))
-
-    time.sleep(15)
-
-    response = browser.find_element_by_xpath("//*").get_attribute("outerHTML")
-
-    getResults(response)
-        
-
-        # browser.find_element_by_id("pagination-current-bottom").send_keys(2)
-        
-        # browser.find_element_by_xpath("*//[@class='pagination-page-jump']").click()
-        # wait.until(EC.invisibility_of_element_located((By.XPATH, "//*[@id='ccpa-button']"))).
-
-        # wait.until(EC.presence_of_element_located((By.XPATH, "//*[@class='next']"))).click()
-
     browser.quit()
+
+
     
     # print(response)
 
 def main():
     getURL()
 
-main()
-sys.exit(0)
+# main()
+# sys.exit(0)
