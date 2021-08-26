157c7a1c4f8a704dda0c3cbbde30e0542a2122ee
Ramon Audain <raudain123@gmail.com>
2021-08-26 11:47:41 -0700

    update json

--- server/job_boards/target.py
+++ server/job_boards/target.py
@@ -51,7 +51,7 @@ def getResults(item):
         position = str(i.find("h2")).replace("<h2>", "").replace("</h2>", "").replace("&amp;", "&").strip()
         locations_string = str(i.find("span", class_="job-location")).replace('<span class="job-location">', "").replace("</span>", "").strip()
         
-        if position != "None" or position != None:
+        if "None" not in position:
             getJobs(date, apply_url, company_name, position, locations_string)
 
 
@@ -59,15 +59,17 @@ def getURL():
     # Add "view-source:" in front of url to avoid firefox autoformatting for json
     url = "view-source:https://jobs.target.com/search-jobs/results?ActiveFacetID=0&CurrentPage=1&RecordsPerPage=500&Distance=50&RadiusUnitType=0&Keywords=&Location=&ShowRadius=False&IsPagination=False&CustomFacetName=&FacetTerm=&FacetType=0&FacetFilters%5B0%5D.ID=67611&FacetFilters%5B0%5D.FacetType=1&FacetFilters%5B0%5D.Count=232&FacetFilters%5B0%5D.Display=Technology+and+Data+Sciences&FacetFilters%5B0%5D.IsApplied=true&FacetFilters%5B0%5D.FieldName=&SearchResultsModuleName=Search+Results&SearchFiltersModuleName=Search+Filters&SortCriteria=0&SortDirection=0&SearchType=6&PostalCode=&fc=&fl=&fcf=&afc=&afl=&afcf="
 
-    browser.get(url)
+    try:
+        browser.get(url)
 
-    response = browser.find_element_by_tag_name("pre").text
+        response = browser.find_element_by_tag_name("pre").text
 
-    data = json.loads(response)
-    browser.quit()
+        data = json.loads(response)
+        browser.quit()
 
-    getResults(data)
-
+        getResults(data)
+    except:
+        print("Error: target failed.")
 
     # print(data)
 
