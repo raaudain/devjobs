776d342541620bf16dab899a34db9f01ed176e65
Ramon Audain <raudain123@gmail.com>
2021-10-08 09:13:21 -0700

    update json

--- server/job_boards/lever_co.py
+++ server/job_boards/lever_co.py
@@ -1,10 +1,10 @@
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 import requests, sys, time, json, random
-from .modules import create_temp_json
-from .modules import headers as h
-# import modules.create_temp_json as create_temp_json
-# import modules.headers as h
+# from .modules import create_temp_json
+# from .modules import headers as h
+import modules.create_temp_json as create_temp_json
+import modules.headers as h
 
 
 data = create_temp_json.data
@@ -16,32 +16,35 @@ f.close()
 
 def getJobs(item, company, source_url):
     for job in item:
-        date = datetime.strftime(datetime.now(), "%Y-%m-%d")
-        title = job.find("h5", {"data-qa": "posting-name"}).text
-        company = company
-        url = job["href"]
-        location = job.find("span", {"class": "sort-by-location posting-category small-category-label"}).text
+        try:
+            date = datetime.strftime(datetime.now(), "%Y-%m-%d")
+            title = job.find("h5", {"data-qa": "posting-name"}).text
+            company = company
+            url = job["href"]
+            location = job.find("span", {"class": "sort-by-location posting-category small-category-label"}).text
 
-        # print(date, title, company, url, location, source_url)
-        postDate = datetime.timestamp(datetime.strptime(date, "%Y-%m-%d"))
+            # print(date, title, company, url, location, source_url)
+            postDate = datetime.timestamp(datetime.strptime(date, "%Y-%m-%d"))
 
-        if url not in scraped:
-            data.append({
-                "timestamp": postDate,
-                "title": title,
-                "company": company,
-                "url": url,
-                "location": location,
-                "source": company,
-                "source_url": source_url,
-                "category": "job"
-            })
-            scraped.add(url)
-            scraped.add(company)
-            print(f"=> lever.co: Added {title} for {company}")
-        else:
-            print(f"=> lever.co: Already scraped {title} for {company}")
-        
+            if url not in scraped:
+                data.append({
+                    "timestamp": postDate,
+                    "title": title,
+                    "company": company,
+                    "url": url,
+                    "location": location,
+                    "source": company,
+                    "source_url": source_url,
+                    "category": "job"
+                })
+                scraped.add(url)
+                scraped.add(company)
+                print(f"=> lever.co: Added {title} for {company}")
+            else:
+                print(f"=> lever.co: Already scraped {title} for {company}")
+        except:
+            print(f"=> lever.co: Scrape failed for {title} - {company}. Going to next.")
+            continue
 
 def getResults(item, name):
     soup = BeautifulSoup(item, "lxml")
@@ -52,13 +55,9 @@ def getResults(item, name):
     postings = []
 
     for result in results:
-        try:
-            h5 = result.find("h5", {"data-qa":"posting-name"}).text
-            if ("Engineer" in h5 or "Tech" in h5 or "Web" in h5 or "Data ") in h5 and ("Pharmacy Tech" not in h5 or "Pharmacy Clerk" not in h5):
-                postings.append(result)
-        except:
-            print(f"=> lever.co: Scrape failed for {name}. Going to next.")
-            continue
+        h5 = result.find("h5", {"data-qa":"posting-name"}).text
+        if ("Engineer" in h5 or "Tech" in h5 or "Web" in h5 or "Data " in h5) and ("Pharmacy Tech" not in h5 or "Pharmacy Clerk" not in h5):
+            postings.append(result)
 
     results = postings
 
@@ -79,7 +78,7 @@ def getURL():
         if response.ok:
             getResults(response.text, name)
         else:
-            print("=> lever.co: Error - Response status", response.status_code)
+            print(f"=> lever.co: Error for {name} - Response status", response.status_code)
         
         if count % 20 == 0:
             time.sleep(5)
@@ -92,5 +91,5 @@ def getURL():
 def main():
     getURL()
 
-# main()
-# sys.exit(0)
+main()
+sys.exit(0)
