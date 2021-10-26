aac11974f082c878ee1eed5578284dc7c3e06a97
Ramon Audain <raudain123@gmail.com>
2021-10-26 12:12:45 -0700

    update json

--- server/job_boards/bloomberg.py
+++ server/job_boards/bloomberg.py
@@ -41,11 +41,13 @@ def get_results(item):
 
 
 def get_url():
-    # headers = {"User-Agent": random.choice(h.headers), "X-Requested-With": "XMLHttpRequest"}
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36", "X-Requested-With": "XMLHttpRequest"}
+    # session = requests.Session()
+    # cookie = session.get("https://careers.bloomberg.com/job/search?&amp;_ga=2.132473930.596821302.1561381910-831768493.1559310775").cookies.get_dict()
+    # print(cookie)
+    headers = {"User-Agent": random.choice(h.headers), "X-Requested-With": "XMLHttpRequest", "Cookie":"l=jfEMhxe6KxjiGjtNRhEjrw.ar-sH2mKPUK-8sPEFAlbsTFcEOhm9N70RygL5ySOGM4skK3s3lXBiGFlBEV8oJGR18K8E4yKEyWkIz3uJVc25w.1635229659446.3600000.4hucrhjxXi9Uqjz6UZWBCPGDgKQYxnACY7IEwQkfXDI"}
+    # headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36", "X-Requested-With": "XMLHttpRequest"}
     url_1 = f"https://careers.bloomberg.com/job_search/search_query?autocompleteTags=%5B%5D&selectedFilterFields=%5B%7B%22name%22%3A%22Software+Developer%2FEngineering%22%2C%22count%22%3A%22239%22%2C%22isSelected%22%3Atrue%2C%22parentFacet%22%3A%22Job+function%22%2C%22id%22%3A%22c41%22%7D%2C%7B%22name%22%3A%22Technical+Support%22%2C%22count%22%3A%2216%22%2C%22isSelected%22%3Atrue%2C%22parentFacet%22%3A%22Job+function%22%2C%22id%22%3A%22c48%22%7D%5D&jobStartIndex=0&jobBatchSize=1000"
     response = requests.get(url_1, headers=headers).text
-    
     data = json.loads(response)
     job_id = []
 
@@ -61,7 +63,7 @@ def get_url():
         if res.ok:
             post = json.loads(res.text)
             # get_results(post)
-            print("hey",url_2)
+            print("hey",post)
             if count % 10 == 0: time.sleep(5)
             count+=1
         else:
