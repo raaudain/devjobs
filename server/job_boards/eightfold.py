e2a30354120cadba40e0c30e0e9f542081e321a5
Ramon Audain <raudain123@gmail.com>
2021-11-26 08:49:25 -0800

    update json

--- server/job_boards/eightfold.py
+++ server/job_boards/eightfold.py
@@ -30,7 +30,7 @@ def get_jobs(date: str, url: str, company: str, position: str, location: str, pa
 
 def get_results(item: str, param: str):
     jobs = item["positions"]
-    company = item["branding"]["companyName"]
+    company = item["branding"]["companyName"] if item["branding"]["companyName"] else param.upper()
 
     for j in jobs:
         if "Engineer" in j["name"] or "Data" in j["name"] or "Support" in j["name"] or "IT" in j["name"] or "Programmer" in j["name"] or "QA" in j["name"] or "Software" in j["name"]  or "Tech " in j["name"] or "Help" in j["name"] or "Desk" in j["name"] or "Developer" in j["name"] and ("Mechnicial" not in j["name"] and "Electrical" not in j["name"] and "Front Desk" not in j["name"] and "Data Entry" not in j["name"] and "Facilities" not in j["name"]):
