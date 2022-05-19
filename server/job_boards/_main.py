664fea808df8b8c3a944e06b8fcded6c8206495c
Ramon Audain <raudain123@gmail.com>
2022-05-18 20:41:58 -0700

    add bootup

--- server/job_boards/_main.py
+++ server/job_boards/_main.py
@@ -39,6 +39,7 @@ from . import fullstackjob
 from . import wrk
 from . import indeed
 from . import diversifytech
+from . import bootup
 # from . import craigslist_gigs
 from datetime import datetime
 # from .modules import createJSON
@@ -78,6 +79,7 @@ def main():
     print("=> Scanning job boards")
     start = datetime.now()
     # bloomberg.main()
+    bootup.main()
     workable.get_url(work[::5])
     diversifytech.main()
     wrk.main()
---
+++ server/job_boards/bootup.py
@@ -0,0 +1,59 @@
+import requests
+import json
+import sys
+import random
+from datetime import datetime
+from .modules import headers as h
+from .modules.classes import Create_JSON, Filter_Jobs
+# import modules.classes as c
+# import modules.headers as h
+
+
+def get_results(item: str):
+    scraped = Create_JSON.scraped
+    date = item["created_at"]
+    post_date = datetime.timestamp(
+        datetime.strptime(str(date), "%Y-%m-%dT%H:%M:%S"))
+    position = item["title"]
+    company_name = item["employer"]["name"]
+    description = item["description"]
+    apply_url = item["url"]
+    location = item["locations"][0]["location"]["city_state"] if len(
+        item["locations"]) > 0 else "See Description"
+    if company_name not in scraped:
+        Filter_Jobs({
+            "timestamp": post_date,
+            "title": position,
+            "company": company_name,
+            "description": description,
+            "company_logo": "https://candidate.joinbootup.com/LogoDarkText.svg",
+            "url": apply_url,
+            "location": location,
+            "source": "Bootup",
+            "source_url": "https://candidate.joinbootup.com/jobs"
+        })
+
+
+def get_url(ids: list):
+    for i in ids:
+        try:
+            headers = {"User-Agent": random.choice(h.headers)}
+            url = f"https://api.joinbootup.com/api/student/jobs/{i}"
+            response = requests.get(url, headers=headers)
+            data = json.loads(response.text)
+            get_results(data["data"])
+        except Exception as e:
+            print(f"=> bootup: Error. {e}")
+
+
+def main():
+    headers = {"User-Agent": random.choice(h.headers)}
+    url = "https://api.joinbootup.com/api/student/jobs?page=1&limit=200"
+    response = requests.get(url, headers=headers)
+    data = json.loads(response.text)["data"]["list"]
+    ids = [e["id"] for e in data]
+    get_url(ids)
+
+
+# main()
+# sys.exit(0)
