240d81e57ceb810a399dbf75cc91555852a1ba49
Ramon Audain <raudain123@gmail.com>
2022-05-25 18:08:35 -0700

    get logos for wrk

--- server/job_boards/wrk.py
+++ server/job_boards/wrk.py
@@ -6,7 +6,7 @@ import random
 from lxml import html
 from datetime import datetime
 from .modules import headers as h
-from .modules.classes import Filter_Jobs, Read_List_Of_Companies, Remove_Not_Found
+from .modules.classes import Filter_Jobs, Get_Stored_Data, Read_List_Of_Companies, Remove_Not_Found
 # import modules.headers as h
 # import modules.classes as c
 
@@ -14,36 +14,45 @@ from .modules.classes import Filter_Jobs, Read_List_Of_Companies, Remove_Not_Fou
 FILE_PATH = "./data/params/wrk.txt"
 
 
-def get_results(item: str, name: str):
-    jobs = item["items"]
-    if jobs:
-        for j in jobs:
-            date = j["published_at"]
-            post_date = datetime.timestamp(
-                datetime.strptime(str(date), "%Y-%m-%dT%H:%M:%S.%fZ"))
-            position = j["title"].strip()
-            company_name = j["organization_name"].strip()
-            apply_url = j["job_post_url"].strip()
-            city = f"{j['city'].strip()}, " if j['city'] else ""
-            state = f"{j['state_region'].strip()}, " if j["state_region"] else ""
-            country = f"{j['country'].strip()}" if j["country"] else ""
-            remote = f" | {j['remoteness_pretty'].strip()}" if j["remoteness_pretty"] and "No" not in j["remoteness_pretty"] else ""
-            location = city+state+country+remote
-            source_url = f"https://jobs.wrk.xyz/{name}"
+def get_results(item: str, param: str):
+    wrk = "../data/assets/wrk_assets.txt"
+    source_url = f"https://jobs.wrk.xyz/{param}"
+    logo = None
+    table = Get_Stored_Data(wrk)
+    if param in table:
+        logo = table[param]["logo"]
+    else:
+        try:
             r = requests.get(source_url)
             tree = html.fromstring(r.content)
-            img = tree.xpath("//div[@class='header__logo']/img/@src")[0]
-            logo = img if img else None
-            Filter_Jobs({
-                "timestamp": post_date,
-                "title": position,
-                "company": company_name,
-                "company_logo": logo,
-                "url": apply_url,
-                "location": location,
-                "source": company_name,
-                "source_url": source_url
-            })
+            logo = tree.xpath("//div[@class='header__logo']/img/@src")[0]
+            with open(wrk, "a") as a:
+                a.write(f"{param}`n/a`{logo}")
+        except Exception as e:
+            print(f"=> wrk: Error getting logo for {param}. {e}.")
+    jobs = item["items"]
+    for j in jobs:
+        date = j["published_at"]
+        post_date = datetime.timestamp(
+            datetime.strptime(str(date), "%Y-%m-%dT%H:%M:%S.%fZ"))
+        position = j["title"].strip()
+        company_name = j["organization_name"].strip()
+        apply_url = j["job_post_url"].strip()
+        city = f"{j['city'].strip()}, " if j['city'] else ""
+        state = f"{j['state_region'].strip()}, " if j["state_region"] else ""
+        country = f"{j['country'].strip()}" if j["country"] else ""
+        remote = f" | {j['remoteness_pretty'].strip()}" if j["remoteness_pretty"] and "No" not in j["remoteness_pretty"] else ""
+        location = city+state+country+remote
+        Filter_Jobs({
+            "timestamp": post_date,
+            "title": position,
+            "company": company_name,
+            "company_logo": logo,
+            "url": apply_url,
+            "location": location,
+            "source": company_name,
+            "source_url": source_url
+        })
 
 
 def get_url(companies: list):
