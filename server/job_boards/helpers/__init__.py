bafed1755b95e74af6b1fb30a8d914f061ff197e
Ramon Audain <raudain123@gmail.com>
2022-11-06 08:48:52 -0800

    update

--- server/job_boards/helpers/__init__.py
+++ server/job_boards/helpers/__init__.py
@@ -1,3 +1,7 @@
+import sys
+sys.path.insert(0, ".")
+
 from server.job_boards.helpers.date_formatter import date_formatter
 from server.job_boards.helpers.classes import ProcessCompanyJobData
 from server.job_boards.helpers import headers
+from server.job_boards.helpers.classes import CreateJson
