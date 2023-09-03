7e80a4c7e85f9f611ed3e9ec6df4303d8daea885
Ramon Audain <raudain123@gmail.com>
2023-09-03 14:17:30 -0700

    remove temp file path

--- src/update_params/utils/data.py
+++ src/update_params/utils/data.py
@@ -1,98 +1,82 @@
 ats = [
     {
-        "file": "./temp/lever_temp.txt",
         "host": "https://jobs.lever.co/",
-        "params": "./src/data/params/lever_co.txt",
+        "params": "src/data/params/lever_co.txt",
         "uri": r"https://jobs.lever.co/(.*?)/"
     },
     {
-        "file": "./temp/greenhouse_temp.txt",
         "host": "https://boards.greenhouse.io/",
-        "params": "./src/data/params/greenhouse_io.txt",
+        "params": "src/data/params/greenhouse_io.txt",
         "uri": r"https://boards.greenhouse.io/(.*?)/"
     },
     {
-        "file": "./temp/polymer_temp.txt",
         "host": "https://jobs.polymer.co/",
-        "params": "./src/data/params/polymer.txt",
+        "params": "src/data/params/polymer.txt",
         "uri": r"https://jobs.polymer.co/(.*?)/"
     },
     {
-        "file": "./temp/jobvite_temp.txt",
         "host": "https://jobs.jobvite.com/",
-        "params": "./src/data/params/jobvite.txt",
+        "params": "src/data/params/jobvite.txt",
         "uri": r"https://jobs.jobvite.com/(.*?)/"
     },
     {
-        "file": "./temp/ashbyhq_temp.txt",
         "host": "https://jobs.ashbyhq.com/",
-        "params": "./src/data/params/ashbyhq.txt",
+        "params": "src/data/params/ashbyhq.txt",
         "uri": r"https://jobs.ashbyhq.com/(.*?)/"
     },
     {
-        "file": "./temp/workable_temp.txt",
         "host": "https://apply.workable.com/",
-        "params": "./src/data/params/workable.txt",
+        "params": "src/data/params/workable.txt",
         "uri": r"https://apply.workable.com/(.*?)/"
     },
     {
-        "file": "./temp/smartrecruiters_temp.txt",
         "host": "https://careers.smartrecruiters.com/",
-        "params": "./src/data/params/smartrecruiters.txt",
+        "params": "src/data/params/smartrecruiters.txt",
         "uri": r"https://careers.smartrecruiters.com/(.*?)/"
     },
     {
-        "file": "./temp/jazzhr_temp.txt",
         "host": ".applytojob.com/",
-        "params": "./src/data/params/jazzhr.txt",
+        "params": "src/data/params/jazzhr.txt",
         "uri": r"https://(.*?).applytojob.com/"
     },
     {
-        "file": "./temp/breezyhr_temp.txt",
         "host": ".breezy.hr/",
-        "params": "./src/data/params/breezyhr.txt",
+        "params": "src/data/params/breezyhr.txt",
         "uri": r"https://(.*?).breezy.hr/"
     },
     {
-        "file": "./temp/recruiterbox_temp.txt",
         "host": ".recruiterbox.com/jobs",
-        "params": "./src/data/params/recruiterbox.txt",
+        "params": "src/data/params/recruiterbox.txt",
         "uri": r"https://(.*?).recruiterbox.com/jobs"
     },
     {
-        "file": "./temp/bamboohr_temp.txt",
         "host": ".bamboohr.com/jobs",
-        "params": "./src/data/params/bamboohr.txt",
+        "params": "src/data/params/bamboohr.txt",
         "uri": r"https://(.*?).bamboohr.com/jobs"
     },
     {
-        "file": "./temp/comeet_temp.txt",
         "host": "https://www.comeet.com/jobs/",
-        "params": "./src/data/params/comeet.txt",
+        "params": "src/data/params/comeet.txt",
         "uri": r"https://www.comeet.com/jobs/((.*?)\/(.*?))/"
     },
     {
-        "file": "./temp/eightfold_temp.txt",
         "host": ".eightfold.ai/careers/",
-        "params": "./src/data/params/eightfold.txt",
+        "params": "src/data/params/eightfold.txt",
         "uri": r"https://(.*?).eightfold.ai/careers/"
     },
     {
-        "file": "./temp/clearcompany_temp.txt",
         "host": ".hrmdirect.com/",
-        "params": "./src/data/params/clearcompany.txt",
+        "params": "src/data/params/clearcompany.txt",
         "uri": r"http://(.*?).hrmdirect.com/"
     },
     {
-        "file": "./temp/recruitee_temp.txt",
         "host": ".recruitee.com/",
-        "params": "./src/data/params/recruitee.txt",
+        "params": "src/data/params/recruitee.txt",
         "uri": r"https://(.*?).recruitee.com/"
     },
     {
-        "file": "./temp/crew_temp.txt",
         "host": ".crew.work/jobs",
-        "params": "./src/data/params/crew.txt",
+        "params": "src/data/params/crew.txt",
         "uri": r"https://(.*?).crew.work/"
     },
 ]
